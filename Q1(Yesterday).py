3e863ab0b805d9400064e3f35d5605a7a6a33e56
RasaghnaKuturu <108987530+rasaghnak@users.noreply.github.com>
2025-07-09 23:57:31 -0400

    Update Q1(Yesterday).py

--- Q1(Yesterday).py
+++ Q1(Yesterday).py
@@ -8,3 +8,29 @@ from datetime import datetime, timedelta
 def yest_date(today)-> datetime:
   yesterday = today - timedelta(days=1)
   return yesterday 
+
+# Description
+# The exams records of students are stored in the exam table
+# Please use SQL statement to find the student_id corresponding to the student with the largest number of failed subjects.
+
+# Table definition: exams
+
+# columns_name	type	explaination
+# id	int unsigned	primary key
+# student_id	int	student's id
+# date	date	date of exam
+# is_pass	int	grade status (0 means fail, others means pass)
+
+
+SELECT student_id
+FROM exams
+WHERE is_pass = 0
+GROUP BY student_id
+HAVING COUNT(*) = (
+        SELECT COUNT(*)
+        FROM exams
+        WHERE is_pass = 0
+        GROUP BY student_id
+        ORDER BY COUNT(*) DESC
+        LIMIT 1
+);
